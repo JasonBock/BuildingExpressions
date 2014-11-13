7fa59ec7df775b1a41ecb2d2af6f6be0ef5b7706
Jason Bock <jason@jasonbock.net>
2014-11-13 07:21:49 -0800

    Updated to VS2015 Ultimate Preview
    Based on the bits from the connect(); event

--- BuildingExpressions.Contracts/Properties/AssemblyInfo.cs
+++ BuildingExpressions.Contracts/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("66fa8aca-7631-4c1d-a89a-86fc93993347")]
+[assembly: Guid("286b594e-29c2-48f9-a6f1-d94d72354489")]
 
 // Version information for an assembly consists of the following four values:
 //
